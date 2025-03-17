1f70c7780256ccbffc219595797f84af8a603125
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-03-17 16:57:54 +0100

    add file sql

---
+++ WEBAPP-EXPRESS/Untitled.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE webapp_express;
+USE webapp_express;
+
+SELECT *
+FROM movies;
+
+SELECT *
+FROM reviews;
+
+SELECT * FROM movies WHERE id=2;
+SELECT * FROM reviews WHERE movie_id= 2;
