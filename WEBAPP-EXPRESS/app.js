7999c9bac7b5891c1b7640a92afcd705b9e8e9f5
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-03-18 18:05:37 +0100

    i axios

--- WEBAPP-EXPRESS/app.js
+++ WEBAPP-EXPRESS/app.js
@@ -1,10 +1,17 @@
 import express from "express";
 
 const app = express();
+import cors from 'cors';
 const port = process.env.SERVER_PORT || 3000;
 
 import movieRouter from './routes/movieRouter.js'
 import imagePathMiddleware from './middlewares/imagePath.js';
+
+app.use(
+    cors({
+      origin: process.env.FRONTEND_APP,
+    })
+  );
 
 app.use( express.static('public') )
 
