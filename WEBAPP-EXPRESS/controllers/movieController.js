8244473ce173b0f15de041d7b7550883deeb9828
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-03-20 15:28:13 +0100

    es

--- WEBAPP-EXPRESS/controllers/movieController.js
+++ WEBAPP-EXPRESS/controllers/movieController.js
@@ -27,7 +27,15 @@ function index(req, res){
 function show(req, res){
     const {id} = req.params;
 
-    const movieSql = 'SELECT * FROM movies WHERE id= ?'
+    // const movieSql = 'SELECT * FROM movies WHERE id= ?'
+
+    const movieSql = `
+        SELECT B.*, ROUND( AVG(R.vote)) AS average_vote
+        FROM movies B
+        LEFT JOIN reviews R
+        ON R.movie_id = B.id
+        WHERE B.id = ?`
+
 
     const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?'
 
@@ -47,24 +55,88 @@ function show(req, res){
                 error: 'Errore lato server SHOW function'
             })
 
-            movie.reviews = reviewsResults
+            // movie.reviews = reviewsResults
             
 
 
             res.json({
                 ...movie,
-                image: req.imagePath + movie.image
+                image: req.imagePath + movie.image,
+                average_vote: parseInt(movie.average_vote),
+                reviews: reviewsResults
             })
-
-            res.json( movie)
 
         })
     }) 
 
 }
 
+function destroy(req, res){
+    const {id} = req.params;
+
+    const sql = 'DELETE FROM movies WHERE id = ?'
+
+    connection.query( sql, [id], (err) => {
+        if(err) return res.status(500).json({
+            error: 'Errore lato server DESTROY function'
+        })
+
+        res.sendStatus( 204 )
+
+    } )
+}
+
+function storeReview(req, res){
+    //recuparare l'id
+    const {id} = req.params
+
+    //recuparare le informazioni del body
+    const {text,name,vote} = req.body
+
+    //preparazione della query
+    const sql = 'INSERT INTO reviews ( text, name, vote, movie_id ) VALUES (?,?,?,?)'
+
+    //eseguiamo la query
+    connection.query( sql, [text, name, vote, id], (err, results) => {
+        if(err) return res.status(500).json({
+            error: 'Database Errore StoreReview'
+        })
+
+        res.status(201)
+        res.json({
+            message: 'review Added',
+            id: results.insertId
+        })
+    } )
+}
+
+function store(req,res){
+    //recuparare le info da req.body
+    const { title, director, abstract} = req.body
+
+    const imageName = `${req.file.filename}`
+
+    const sql = "INSERT INTO movies (title, director, image, abstract) VALUES (?,?,?,?)"
+
+    connection.query( sql, [title, director, imageName, abstract], (err, results) => {
+        if(err) return res.status(500).json({
+            error: 'Database Errore Store'
+        })
+
+        res.status(201).json({
+            status: "success",
+            message: "Film creato con successo",
+            id: results.insertId
+        }
+        )
+    })
+
+}
 
 export {
     index,
-    show
+    show,
+    destroy,
+    storeReview,
+    store
 }
--- WEBAPP-EXPRESS/routes/movieRouter.js
+++ WEBAPP-EXPRESS/routes/movieRouter.js
@@ -1,11 +1,19 @@
 import express from 'express'
 
+import upload from '../middlewares/multer.js'
+
 const router = express.Router()
 
-import {index, show } from '../controllers/movieController.js'
+import {index, show, destroy, storeReview, store } from '../controllers/movieController.js'
 
 router.get( '/', index );
 
-router.get( '/:id', show )
+router.get('/:id', show)
+
+router.delete('/:id', destroy);
+
+router.post('/:id/reviews', storeReview);
+
+router.post( '/', upload.single('image'), store )
 
 export default router
